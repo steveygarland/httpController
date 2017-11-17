daafd6338e0e08bf7a3c917b7f6f5bdab57f39a5
steveygarland <stevey.garland@gmail.com>
2017-11-17 12:51:45 +0000

    Fixed bug in repo.go where after process was killed outside of httpController no further commands would be accepted

--- repo.go
+++ repo.go
@@ -29,7 +29,7 @@ var args string
 var cmd string
 var todos Todos
 var apps Apps
-
+var count int
 
 // Give us some seed data
 func init() {
@@ -47,7 +47,7 @@ func RepoAppControl(a App) App {
       b, _ := json.Marshal(a)
       s := string(b)
 
-
+     cmd := exec.Command(processName, arg1)
      if (state == "started"){
 
 
@@ -60,7 +60,6 @@ func RepoAppControl(a App) App {
 
 
 
-      cmd := exec.Command(processName, arg1)
       cmd.Stdout = os.Stdout 
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
       err := cmd.Start()
@@ -77,13 +76,13 @@ func RepoAppControl(a App) App {
 	fmt.Println("Error:", err)
         fmt.Println("Status:", exitStatus)
 	if exitStatus == -1 {
-//	for{
+	count :=1
+	for i :=1; i <= count; i++ {
 	if  (state != "stopped" ) {
 	fmt.Println("Process : ", processName, "stopped outside of controller: Restarting")
 	cmd := exec.Command(processName, arg1)
         cmd.Stdout = os.Stdout
         cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-      	//args :=  []string{"-i"}
       	err := cmd.Start()
       	if err !=nil {
                 fmt.Fprintln(os.Stderr, err)
@@ -93,17 +92,16 @@ func RepoAppControl(a App) App {
         runningPid = cmd.Process.Pid
         fmt.Println("Application Successfully Restarted!:", cmd )
         fmt.Println("Running:" , cmd.Process.Pid)
+	count++
 	cmd.Wait() 
 }
-//	}
 	}
-       // close(done)
+}
+	
 
 } else if (state == "stopped" ) {
-	isProcessRunning(runningPid)
 	fmt.Println("Stopping:" , processName)
 	fmt.Println("Stopping PID: ", runningPid)
-        //syscall.Kill(runningPid, syscall.SIGKILL)
 	args = processName
 	err2 := filepath.Walk("/proc", findAndKillProcess)
         if err2 != nil {
@@ -115,98 +113,23 @@ func RepoAppControl(a App) App {
 
 }
 }
+
 }
       return a
 }
 
 
-
-
-
-
-
-func findProcess(path string, info os.FileInfo, err error) error {
-    if err != nil {
-        return nil
-    }
-    processName := path
-    fmt.Printf("findProcess:: PROCESSNAME1::%s\n", path)
-
-    if strings.Count(path, "/") == 3 {
-        if strings.Contains(path, "/status") {
-
-            pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
-                 fmt.Printf("TEST::%d", pid)
-            if err != nil {
-                log.Println(err)
-                return nil
-            }
-
-            f, err := ioutil.ReadFile(path)
-            if err != nil {
-                log.Println(err)
-                return nil
-            }
-
-            name := string(f[6:bytes.IndexByte(f, '\n')])
-                        fmt.Printf("findProcess:: PROCESSNAME::%s NAME::%s PATH::%s PID::%d \n",processName, name, path, pid)
-            if name == args {
-                fmt.Printf("PID: %d, Name: %s is running.\n", pid, name)
-                proc, err := os.FindProcess(pid)
-                if err != nil {
-                    log.Println(err)
-                }
-                fmt.Printf("PROC %d\n", proc)
-                pgid, err := syscall.Getpgid(pid)
-                fmt.Printf("PGID %d\n", pgid)
-                if err == nil {
-		fmt.Printf("PID: %d, Name: %s is running.\n", pid, name)
-
-                }
-                return io.EOF
-            }
-
-        }
-    }
- return nil
-}
-
-
-
-func stopProcess(pid string) string{
-	pidToKill, err := strconv.Atoi(pid)
-	fmt.Sprintf("%d", pidToKill)
-	if err !=nil {
-		fmt.Sprintf("Failed to kill: %s ", err)
-	}
-	//fmt.Sprintf("%d", pidToKill)
-	syscall.Kill(pidToKill, syscall.SIGTERM)
-	return (pid) 
-}
-
-func isProcessRunning(pid int) bool {
-        process, err := os.FindProcess(pid)
-        if err != nil {
-            fmt.Printf("Failed to find process: %s\n", err)
-	    return false
-        } else {
-            err := process.Signal(syscall.Signal(0))
-            fmt.Printf("process.Signal on pid %d returned: %v\n", pid, err)
-	    return true
-        }
-}
-
 func findAndKillProcess(path string, info os.FileInfo, err error) error {
     if err != nil {
         return nil
     }
-    processName := path
+    //processName := path
 
     if strings.Count(path, "/") == 3 {
         if strings.Contains(path, "/status") {
 
             pid, err := strconv.Atoi(path[6:strings.LastIndex(path, "/")])
-		 fmt.Printf("TEST::%d", pid)
+		 //fmt.Printf("TEST::%d", pid)
             if err != nil {
                 log.Println(err)
                 return nil
@@ -219,7 +142,7 @@ func findAndKillProcess(path string, info os.FileInfo, err error) error {
             }
 
             name := string(f[6:bytes.IndexByte(f, '\n')])
-			fmt.Printf("PROCESSNAME::%s NAME::%s PATH::%s PID::%d \n",processName, name, path, pid)
+			//fmt.Printf("PROCESSNAME::%s NAME::%s PATH::%s PID::%d \n",processName, name, path, pid)
             if name == args {
                 fmt.Printf("PID: %d, Name: %s will be killed.\n", pid, name)
                 proc, err := os.FindProcess(pid)
